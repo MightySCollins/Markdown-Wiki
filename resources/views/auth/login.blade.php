6eb2f1b76f335ae50132e05323998efbaf363b07
Sam Collins <theatrepro11@gmail.com>
2016-03-19 00:19:09 +0000

    Started Boostrap 4 Login Page styling and setup SASS Intergration

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,63 @@
+@extends('layouts.app')
+
+@section('content')
+
+<form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
+  {!! csrf_field() !!}
+  
+  @if ($errors->has('email'))
+  <div class="form-group row has-danger">
+    <label for="email" class="col-sm-2 form-control-label">Email</label>
+    <div class="col-sm-10">
+      <input type="email" class="form-control form-control-danger" id="email"
+        name="email" value="{{ old('email') }}" placeholder="Email">
+      <span class="text-help">{{ $errors->first('email') }}</span>
+    </div>
+  </div>
+  @else
+  <div class="form-group row">
+    <label for="email" class="col-sm-2 form-control-label">Email</label>
+    <div class="col-sm-10">
+      <input type="email" class="form-control" id="email" name="email"
+        value="{{ old('email') }}" placeholder="Email">
+    </div>
+  </div>
+  @endif
+  
+  @if ($errors->has('password'))
+  <div class="form-group row has-danger">
+    <label for="password" class="col-sm-2 form-control-label">Password</label>
+    <div class="col-sm-10">
+      <input type="password" class="form-control form-control-danger" id="password"
+        name="password" value="{{ old('password') }}" placeholder="Password">
+      <span class="text-help">{{ $errors->first('password') }}</span>
+    </div>
+  </div>
+  @else
+  <div class="form-group row">
+    <label for="password" class="col-sm-2 form-control-label">Password</label>
+    <div class="col-sm-10">
+      <input type="password" class="form-control" id="password" name="password"
+        value="{{ old('password') }}" placeholder="Password">
+    </div>
+  </div>
+  @endif
+  
+  <div class="form-group row">
+    <div class="col-sm-10 col-md-offset-2">
+      <div class="checkbox">
+        <label>
+          <input type="checkbox"  name="remember"> Remember Me
+        </label>
+      </div>
+    </div>
+  </div>
+  
+  <div class="form-group row">
+    <div class="col-sm-offset-2 col-sm-10">
+      <button type="submit" class="btn btn-secondary">Sign in</button>
+    </div>
+    <a class="btn btn-link" href="{{ url('/password/reset') }}">Forgot Your Password?</a>
+  </div>
+</form>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Markdown Wiki</title>
+
+    <link href="{{ elixir('css/app.css') }}" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <!-- Fixed navbar -->
+    <div class="pos-f-t">
+      <div class="collapse" id="navbar-header">
+        <div class="container bg-inverse p-a-1">
+          <h3>Collapsed content</h3>
+          <p>Toggleable via the navbar brand.</p>
+        </div>
+      </div>
+      <nav class="navbar navbar-light navbar-static-top bg-faded">
+        <div class="container">
+          <button class="navbar-toggler hidden-sm-up" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar2">
+            &#9776;
+          </button>
+          <div class="collapse navbar-toggleable-xs" id="exCollapsingNavbar2">
+            <a class="navbar-brand" href="#">Sticky footer</a>
+            <ul class="nav navbar-nav">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Nav item <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Another nav item</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">More nav</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Last link</a>
+              </li>
+              
+              @if (Auth::guest())
+                <li class="nav-item">
+                  <a class="nav-link" href="{{ url('/login') }}">Login</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="{{ url('/register') }}">Register</a>
+                </li>
+              @else
+                <li class="nav-item">
+                  <a class="nav-link" href="{{ url('/profile') }}">Welcome {{ Auth::user()->name }}</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="{{ url('/logout') }}">Logout</a>
+                </li>
+              @endif
+              
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+
+    <div class="container">
+    @yield('content')
+    </div>
+    
+    <footer class="footer">
+      <div class="container">
+        <span class="text-muted">Markdown Wiki - In development</span>
+      </div>
+    </footer>
+
+    <script src="{{ elixir('js/app.js') }}"></script>
+  </body>
+</html>
